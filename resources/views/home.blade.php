fdd4be19117444b69d71f9e48af392dda72ebfdc
Veronica789 <karla.coreas23@itca.edu.sv>
2024-09-27 17:51:08 -0600

    actualizacion de app blade y home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,38 @@
-{{-- Heredamos la estructura del archivo app.blade.php --}}
 @extends('layout.app')
 
-{{-- Definimos el titulo --}}
+{{-- Definimos el título --}}
 @section('title', 'Inicio')
     
 {{-- Definimos el contenido --}}
 @section('content')
-
-    <h1><b>{{$nombre}}</b><br></h1>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card text-center">
+                    <div class="card-header bg-dark text-white">
+                        <h2>Bienvenido al Sistema de Ventas</h2>
+                    </div>
+                    <div class="card-body">
+                        <h4 class="card-title">Hola, <b>{{$nombre}}</b></h4>
+                        <p class="card-text">Gestiona tus ventas, productos, clientes, pedidos y más, de manera eficiente y rápida.</p>
+                        <p>Accede a las diferentes secciones para comenzar:</p>
+                        <div class="d-flex justify-content-around">
+                            <a href="/productos" class="btn btn-primary">
+                                <i class="fas fa-box"></i> Productos
+                            </a>
+                            <a href="/clientes" class="btn btn-success">
+                                <i class="fas fa-users"></i> Clientes
+                            </a>
+                            <a href="/pedidos" class="btn btn-warning text-white">
+                                <i class="fas fa-shopping-cart"></i> Pedidos
+                            </a>
+                        </div>
+                    </div>
+                    <div class="card-footer text-muted">
+                        Sistema de Ventas - Gestiona tu negocio fácilmente
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,176 +7,166 @@
     <title>@yield('title')</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body style="background-color:darkcyan">
-    {{-- Nuestro menú --}}
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark mb-4">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Sistema de Ventas</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link active" aria-current="page" href="/">Inicio</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+<body>
+    {{-- Menú vertical --}}
+    <div class="d-flex">
+        <nav class="bg-dark navbar-dark vh-100 p-3" style="width: 262px;">
+            <a class="navbar-brand text-white mb-4" href="#">Sistema de Ventas</a>
+            <ul class="nav nav-pills flex-column">
+                <li class="nav-item mb-2">
+                    <a class="nav-link text-white {{ Request::is('/') ? 'active' : '' }}" href="/">Inicio</a>
+                </li>
+                <li class="nav-item dropdown mb-2">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Accesos
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                        <li><a class="dropdown-item" href="#">Crear</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown mb-2">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Categorias
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                        <li><a class="dropdown-item" href="#">Crear</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown mb-2">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Menus
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                        <li><a class="dropdown-item" href="#">Crear</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown mb-2">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Platos
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                        <li><a class="dropdown-item" href="#">Crear</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown mb-2">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Productos
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                        <li><a class="dropdown-item" href="#">Crear</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown mb-2">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Empleados
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                        <li><a class="dropdown-item" href="#">Crear</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown mb-2">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Clientes
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                        <li><a class="dropdown-item" href="#">Crear</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown mb-2">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Mesas
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                        <li><a class="dropdown-item" href="#">Crear</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown mb-2">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Promociones
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Pedidos
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Detalle de Pedidos
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Reservaciones
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Pagos
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Informes
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Detalle de Informes
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Mostrar</a></li>
-                            <li><a class="dropdown-item" href="#">Crear</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <div class="container-fluid">
-        @yield('content')
-    </div>
-      @yield('scripts')
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                        <li><a class="dropdown-item" href="#">Crear</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown mb-2">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Pedidos y Detalle de Pedidos
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-submenu">
+                            <a class="dropdown-item dropdown-toggle" href="#">Pedidos</a>
+                            <ul class="dropdown-submenu">
+                                <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                                <li><a class="dropdown-item" href="#">Crear</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown-submenu">
+                            <a class="dropdown-item dropdown-toggle" href="#">Detalle de Pedidos</a>
+                            <ul class="dropdown-submenu">
+                                <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                                <li><a class="dropdown-item" href="#">Crear</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown mb-2">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Reservaciones y pagos
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-submenu">
+                            <a class="dropdown-item dropdown-toggle" href="#">Reservaciones</a>
+                            <ul class="dropdown-submenu">
+                                <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                                <li><a class="dropdown-item" href="#">Crear</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown-submenu">
+                            <a class="dropdown-item dropdown-toggle" href="#">Pagos</a>
+                            <ul class="dropdown-submenu">
+                                <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                                <li><a class="dropdown-item" href="#">Crear</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown mb-2">
+                    <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Informes y Detalle de Informes
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li class="dropdown-submenu">
+                            <a class="dropdown-item dropdown-toggle" href="#">Informes</a>
+                            <ul class="dropdown-submenu">
+                                <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                                <li><a class="dropdown-item" href="#">Crear</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown-submenu">
+                            <a class="dropdown-item dropdown-toggle" href="#">Detalle de Informes</a>
+                            <ul class="dropdown-submenu">
+                                <li><a class="dropdown-item" href="#">Mostrar</a></li>
+                                <li><a class="dropdown-item" href="#">Crear</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+        </nav>
 
+        <div class="container-fluid p-4">
+            @yield('content')
+        </div>
+    </div>
+
+    @yield('scripts')
 </body>
 </html>
